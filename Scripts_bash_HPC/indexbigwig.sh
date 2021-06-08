d8bbff76ff90114a76bd78714afabb2f7e2106b8
monolo90 <50148768+monolo90@users.noreply.github.com>
2021-06-08 10:16:39 +0200

    Scripts para el procesado desde FASTQ hasta BigWIG

--- Scripts_bash_HPC/indexbigwig.sh
+++ Scripts_bash_HPC/indexbigwig.sh
@@ -9,5 +9,5 @@ samtools index ${day%}.H3K27ac.final.bam
 
 bamCoverage -b ${day%}.H3K27ac.final.bam -o ${day%}.H3K27ac.bigwig -of bigwig --normalizeUsing RPGC --effectiveGenomeSize 2700000000 --numberOfProcessors max/2
 
-echo "FINISH ------------------------------"
+echo "FINISH -------------------------------"
 
